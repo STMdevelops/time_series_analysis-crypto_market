146d352ee32941436d3d6311c89c5a690fc7c4e6
STMDevelops <stmdevelops@gmail.com>
2026-01-04 15:30:27 +0000

    Initial commit

---
+++ time_series_analysis_crypto_market.py
@@ -0,0 +1,22 @@
+#%%
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cpi
+from statsmodels.tsa.stattools import adfuller, acf, pacf
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+import pmdarima as pm
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+#%%
+btc = yf.Ticker("BTC-GBP")
+eth = yf.Ticker("ETH-GBP")
+
+btc.info
+
+#%%
+eth.info
