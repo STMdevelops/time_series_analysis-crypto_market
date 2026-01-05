c5785e70bce24da81dc66ac6cd7c496d0a359d9d
STMDevelops <stmdevelops@gmail.com>
2026-01-05 19:42:48 +0000

    Updates

--- time_series_analysis_crypto_market.py
+++ time_series_analysis_crypto_market.py
@@ -20,3 +20,13 @@ btc.info
 
 #%%
 eth.info
+
+#%%
+data_btc = yf.download(["BTC-GBP", "^DJI"], start="2012-01-01", end="2026-01-01")
+data_eth = yf.download(["ETH-GBP", "^DJI"], start="2016-01-01", end="2026-01-01")
+
+# %%
+data_btc
+
+#%%
+data_eth
