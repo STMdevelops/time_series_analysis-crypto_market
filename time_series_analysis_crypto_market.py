8490e4c6f6db5fdf15d4f3573200b978c630fb81
STMDevelops <stmdevelops@gmail.com>
2026-01-07 23:58:36 +0000

    Updates

--- time_series_analysis_crypto_market.py
+++ time_series_analysis_crypto_market.py
@@ -42,12 +42,27 @@ data_eth.Close["ETH-GBP"].plot(title="ETH/GBP");
 #%%
 # --------------------------- Adjust Data for Inflation ---------------------------
 cpi.update()
-close1 = data_btc.copy()
-close1.rename(columns={"^DJI" : "DJI"}, inplace=True)
-close1
+close_btc = data_btc.copy()
+close_btc.rename(columns={"^DJI" : "DJI"}, inplace=True)
+close_btc
 
 #%%
 cpi.update()
-close2 = data_eth.Close.copy()
-close2.rename(columns={"^DJI" : "DJI"}, inplace=True)
-close2
+close_eth = data_eth.Close.copy()
+close_eth.rename(columns={"^DJI" : "DJI"}, inplace=True)
+close_eth
+
+#%%
+date_range_btc = pd.date_range(close_btc.index[0] - pd.DateOffset(days=3),
+                               close_btc.index[-1], freq="MS")
+date_range_btc
+
+#%%
+date_range_eth = pd.date_range(close_eth.index[0], pd.DateOffset(days=3),
+                               close_eth.index[-1], freq="MS")
+date_range_eth
+
+#%%
+cpi_mon_btc = pd.DataFrame(index=date_range_btc)  # check if this is needed
+cpi_mon_eth = pd.DataFrame(index=date_range_eth) # check if this is needed
+
