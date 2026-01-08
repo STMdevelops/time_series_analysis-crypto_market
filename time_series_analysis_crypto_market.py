cb11ea47a04a6b8707f823b8f8774b70e1807e4f
STMDevelops <stmdevelops@gmail.com>
2026-01-08 21:27:16 +0000

    Updates

--- time_series_analysis_crypto_market.py
+++ time_series_analysis_crypto_market.py
@@ -11,6 +11,7 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 import pmdarima as pm
 from sklearn.metrics import mean_squared_error
 from math import sqrt
+from datetime import date
 
 #%%
 # --------------------------- Select Data via API ---------------------------
@@ -58,7 +59,7 @@ date_range_btc = pd.date_range(close_btc.index[0] - pd.DateOffset(days=3),
 date_range_btc
 
 #%%
-date_range_eth = pd.date_range(close_eth.index[0], pd.DateOffset(days=3),
+date_range_eth = pd.date_range(close_eth.index[0] - pd.DateOffset(days=3),
                                close_eth.index[-1], freq="MS")
 date_range_eth
 
@@ -66,3 +67,34 @@ date_range_eth
 cpi_mon_btc = pd.DataFrame(index=date_range_btc)  # check if this is needed
 cpi_mon_eth = pd.DataFrame(index=date_range_eth) # check if this is needed
 
+#%%
+cpi_btc_202301 = cpi.get(date(2023,1,1))
+cpi_btc_202301
+
+#%%
+cpi_eth_202301 = cpi.get(date(2023,1,1))
+cpi_eth_202301
+
+#%%
+cpi_mon_btc['factor'] = cpi_mon_btc.index.map(lambda x: cpi_btc_202301 / cpi.get(x.date()))
+cpi_mon_eth['factor'] = cpi_mon_eth.index.map(lambda x: cpi_eth_202301 / cpi.get(x.date()))
+
+#%%
+cpi_mon_btc['factor'] = close_btc.index.to_period('M').map(cpi_mon_btc.factor.to_period('M').to_dict())
+cpi_mon_eth['factor'] = close_eth.index.to_period('M').map(cpi_mon_eth.factor.to_period('M').to_dict())
+
+#%%
+cols_btc = ['BTC-GBP', 'DJI']
+for col in cols_btc:
+    close_btc[f'{col}_scaled'] = close_btc[col] * close_btc.factor
+close_btc
+
+#%%
+cols_eth = ['ETH-GBP', 'DJI']
+for col in cols_eth:
+    close_eth[f'{col}_scaled'] = close_eth[col] * close_eth.factor
+close_eth
+
+#%%
+close_btc.cols_eth['BTC-GBP_scaled'].plot()
+close_btc.cols_eth['BTC-GBP_scaled'].plot()
